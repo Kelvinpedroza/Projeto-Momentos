26493269221c41b706be553688571a3be9b6abfd
Kelvin Pedroza <109870376+Kelvinpedroza@users.noreply.github.com>
2023-01-07 17:19:01 -0300

    Estruturação dos components header e footer

--- moments/src/app/app-routing.module.ts
+++ moments/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path:'about',
     component: AboutComponent
+  },
+  {
+    path:'new',
+    component: NewMomentComponent
   }
 ];
 
