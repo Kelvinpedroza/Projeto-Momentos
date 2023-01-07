8878a30c18016536d57718b60475a046a7e824c5
Kelvin Pedroza <109870376+Kelvinpedroza@users.noreply.github.com>
2023-01-07 19:58:47 -0300

    Criação das mensagens de error

--- moments/src/app/components/moments-form/moments-form.component.ts
+++ moments/src/app/components/moments-form/moments-form.component.ts
@@ -1,11 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moments-form',
   templateUrl: './moments-form.component.html',
-  styleUrls: ['./moments-form.component.css']
+  styleUrls: ['./moments-form.component.css'],
 })
-export class MomentsFormComponent {
-  @Input() btnText! : string
+export class MomentsFormComponent implements OnInit {
+  @Input() btnText!: string;
 
+  momentForm!: FormGroup;
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      image: new FormControl(''),
+    });
+  }
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if(this.momentForm.invalid){
+      return
+    }
+    console.log('enviado')
+  }
 }
